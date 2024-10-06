4748f229eb04db0ed2cb4c1d3381e6ccd8101ee3
damianodionori <d.dionori@gmail.com>
2024-10-06 17:06:27 +0100

    process env

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
